6830bf5f50598c2ac23bb1637485eedb9055c472
Epalma98 <elijahpalma98@yahoo.com>
2023-03-09 22:18:38 -0800

    Updated live port

--- server.js
+++ server.js
@@ -25,3 +25,7 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.post('/api/notes')
+
+app.listen(process.env.PORT || 3001, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
